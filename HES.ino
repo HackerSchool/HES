0302fde20fa1b02e3a8f5989c7c4f0a8a5d7fe7b
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2016-02-07 16:05:52 +0000

    Improves arduino code. Key diff with debounce

--- HES.ino
+++ HES.ino
@@ -1,18 +1,48 @@
+#define PIN_START   0
+#define PIN_SELECT  1
+#define PIN_A       2
+#define PIN_B       3
+#define PIN_UP      4
+#define PIN_RIGHT   5
+#define PIN_DOWN    6
+#define PIN_LEFT    7
 
 void setup()
 {
     Serial.begin(9600);
+
+    // activate pull-up resistors
+    PORTD = ~(char)0;
 }
+
+unsigned char prev, curr=~0, diff;
 
 void loop()
 {
-    Serial.print(0);
-    Serial.print(1);
-    Serial.print(2);
-    Serial.print(3);
-    Serial.print(4);
-    Serial.print(5);
-    Serial.print(6);
-    Serial.print(7);
-    delay(1000);
+    prev = curr;
+    curr = PIND;
+    diff = curr^prev;
+
+    for(int i=0; i<8; i++)
+    {
+        if(diff>>i & 1)
+        {
+            //Serial.println(i);
+            if(curr>>i & 1) // key up
+            {
+                Serial.write(8 | i);
+            }
+            else    // key down
+            {
+                Serial.write(i);
+            }
+        }
+    }
+
+    //Serial.print(prev, BIN);
+    //Serial.print(' ');
+    //Serial.print(curr, BIN);
+    //Serial.print(' ');
+    //Serial.println(diff, BIN);
+    delay(10);
 }
--- driver.py
+++ driver.py
@@ -1,19 +1,31 @@
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
-conv = {'0': 'start',
-        '1': 'select',
-        '2': 'A',
-        '3': 'B',
-        '4': 'up',
-        '5': 'right',
-        '6': 'down',
-        '7': 'left',
+conv = {0: 'start',
+        1: 'select',
+        2: 'A',
+        3: 'B',
+        4: 'up',
+        5: 'right',
+        6: 'down',
+        7: 'left',
         }
 
 while True:
+    # read a single code from the controller
     code = ser.read()
-    code = code.decode('ascii')
+    # convert it to an int
+    #code = code.decode('ascii')
+    code = int.from_bytes(code, byteorder='little')
 
-    print(conv[code])
+    # decompose the code in parts
+    updown = code>>3 & 1
+    btnid = code & 7
+
+    print(updown, btnid)
+    if(updown):
+        print(conv[btnid], "up")
+    else:
+        print(conv[btnid], "down")
+
