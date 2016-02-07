65464bdb46ea36706a533b8d2c1eb45b3ca145e0
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2016-02-07 15:58:19 +0000

    readability improvements

--- Computer/keyboard_emulation.py
+++ Computer/keyboard_emulation.py
@@ -6,22 +6,12 @@ from time import sleep
 from controller_communication import HESInterface
 from button_translation import translate_button
 
-HES = HESInterface()
 
-while True:
-    key = translate_button(HES.read_data())
-    if key:
-        pyautogui.press(key)
-        key = None
-        sleep(0.1)
-
-if __name__ == '__main__':
-
-    sleep(1)
-    pyautogui.typewrite('Hello world!', interval=0)
-    pyautogui.typewrite('Hello world!', interval=0.1)
-    pyautogui.press('left')
-    pyautogui.keyDown('shift')
-    pyautogui.typewrite(['left', 'left', 'left', 'left', 'left', 'left'])
-    pyautogui.keyUp('shift')
-    # pyautogui.hotkey('ctrl', 'shift', 'esc')
+pyautogui.PAUSE = 0
+with HESInterface() as controller:
+    while True:
+        key = translate_button(controller.read_data())
+        if key:
+            pyautogui.press('a')
+            key = None
+            sleep(0.1)
